2e997d744925293cab755196f9c487db79e9ed45
Gladston de Paula <gladston.dev@gmail.com>
2021-04-17 13:08:02 -0300

    :package: Smoth Scrolling deployed

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 document.querySelector(".hamburger").addEventListener("click", function(){
     document.querySelector(".container").classList.toggle("show-menu");
 });
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
